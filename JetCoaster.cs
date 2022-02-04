a16b3b29b0761afb26f1b21d589d0a621593b5b2
伊久弥 田原 <190077@jc-21.jp>
2022-02-04 10:49:31 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class JetCoaster
+    {
+
+        public static int ride(int k,int[] g ,int r)
+        {
+            int deq = 0;            //deqの取り出し先
+            int mid = 0;            //途中経過
+            int result = 0;         //結果     
+            int peek = 0;           //peekの取り出し先
+            int tem = 0;            //over判定
+
+            var que = new Queue<int>();
+            
+            for(int s = 0; s < g.Length; s++)
+            {
+                que.Enqueue(g[s]);            
+            }
+
+            for (int i  = 0;i < r;i++){
+
+                for (int j = 0 ; j < k; )
+                {                
+                    peek = que.Peek();
+                    tem += peek;
+                    
+                    if (tem > k)
+                    {
+                        tem = 0;
+                        break;
+                    }
+
+                    deq = que.Dequeue();
+                    j += deq;
+                    mid = j;
+                
+                    if(j > k)
+                    {
+                        mid -= deq;
+                        break;
+                    }
+                    que.Enqueue(deq);
+                }
+
+                result += mid;
+            }
+            return result;
+        }
+
+    }
+}
